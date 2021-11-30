a820b0a54a5e1fc8fac4819d9992727f8e6c9b3c
Vamshi Molla <vamshimolla@Vamshis-MacBook-Air.local>
2021-11-30 13:05:40 +0530

    Create a class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './Components/Greeting';
+import Welcome from './Components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
       <Greet></Greet>
+      <Welcome></Welcome>
     </div>
   );
 }
