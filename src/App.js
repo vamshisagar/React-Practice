8df5a741c9ef435c8483d8891dd6cf28b280b43e
Vamshi Molla <vamshimolla@Vamshis-MacBook-Air.local>
2021-11-30 12:50:09 +0530

    Create a functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './Components/Greeting';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Greet></Greet>
     </div>
   );
 }
---
+++ src/Components/Greeting.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// function Greet() {
+//     return <h1>Hello Vamshi</h1>
+// }
+
+//using arrow syntax
+const Greet = () =>  <h1>Hello vamshi</h1>
+
+export default Greet
